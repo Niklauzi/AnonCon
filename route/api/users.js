919bd2e08bc018d13db0040674fba7cd9c607797
Niklauzi <ftpfavour@gmail.com>
2023-08-07 14:25:30 +0100

    adding previous progress

--- route/api/users.js
+++ route/api/users.js
@@ -31,9 +31,10 @@ router.post(
       let user = await User.findOne({ email });
 
       if (user) {
-        return res
-          .status(400)
-          .json({ errors: [{ msg: 'User already exists' }] });
+        return user;
+        // res
+        //   .status(400)
+        //   .json({ errors: [{ msg: 'User already exists' }] });
       }
       //  Get users gravatar
 
@@ -57,7 +58,7 @@ router.post(
       await user.save();
 
       //  Return jsonwebtoken
-      res.send('User registered great');
+      res.send('User registered');
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
